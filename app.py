8043feaa67e967f3f41b095e589930bf9c5f4484
bolicoding <bolicoding@gmail.com>
2020-12-02 08:33:29 -0700

    working python signup server

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  return 'hello world'
-
+CORS(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(500), unique=False)
+    def __init__(self, email):
+        self.email = email
+class EmailSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'email')
+email_schema = EmailSchema()
+emails_schema = EmailSchema(many=True)
+@app.route('/sign-up', methods=["POST"])
+def add_quote():
+    email = request.json['email']
+    new_email = Email(email)
+    db.session.add(new_email)
+    db.session.commit()
+    email = Email.query.get(new_email.id)
+    return email_schema.jsonify(email)
+@app.route("/get-emails", methods=["GET"])
+def get_quotes():
+    all_emails = Email.query.all()
+    result = emails_schema.dump(all_emails)
+    return jsonify(result)
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,7 @@
+- Plan for Today:
+- create a database to take in emails.
+
+  - POST
+  - GET (to make sure it works)
+
+- Make contact form
