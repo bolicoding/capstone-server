30a8edeb353de61f22d81c0ff82d147bc6f3855b
bolicoding <bolicoding@gmail.com>
2020-12-03 16:23:56 -0700

    fixed database uri

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ app.config.update(
 )
 mail = Mail(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
